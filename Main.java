ee084ae4842f5291430b29acc814e4ad672d0009
Chica31 <valentinachicaaguirre@gmail.com>
2025-10-05 20:02:10 -0500

    Se agrera el menú que se mostrará en consola

--- Main.java
+++ Main.java
@@ -57,3 +57,50 @@ class ArbolBinario {
                 : buscarRec(nodo.izquierdo, valor);
     }
 }
+//Menú en consola
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArbolBinario arbol = new ArbolBinario();
+        int opcion, numero;
+
+        do {
+            System.out.println("\n--- MENÚ ÁRBOL BINARIO ---");
+            System.out.println("1. Insertar número");
+            System.out.println("2. Mostrar recorrido");
+            System.out.println("3. Buscar número");
+            System.out.println("4. Salir");
+            System.out.print("Elige una opción: ");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.print("Ingresa el número: ");
+                    numero = sc.nextInt();
+                    arbol.insertar(numero);
+                    System.out.println("Número inseratado.");
+                    break;
+                case 2:
+                    System.out.println("Recorrido inorden: ");
+                    arbol.inorden();
+                    break;
+                case 3:
+                    System.out.println("Ingrese el número que desea buscar: ");
+                    numero = sc.nextInt();
+                    if (arbol.buscar(numero)) {
+                        System.out.println("El número " + numero + " SÍ existe en el árbol.");
+                    } else {
+                        System.out.println("El número " + numero + " NO existe en el árbol. ");
+                    }
+                    break;
+                case 4:
+                    System.out.println("--- Saliendo ---");
+                    break;
+                default:
+                    System.out.println("Opción no válida. ");
+            }
+        } while ( opcion != 4) ;
+        sc.close();
+
+    }
+}
